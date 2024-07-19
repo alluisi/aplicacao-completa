f631cbbc11ac5a9608335dc6ce0ed56e81e63baf
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-18 22:45:44 -0300

    Conceitos do sistema de navegação

---
+++ esquenta/1-masterclass-react-next/src/app/temp/a/b/c/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Temp - a - b - c</h1>
+            <nav>
+                <Link href={"/"}>Início</Link>
+            </nav>
+        </div>
+    )
+}
