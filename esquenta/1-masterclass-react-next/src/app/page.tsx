e35c026268d8497fb16c047fa09f7c74bdd3b035
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-19 22:21:21 -0300

    Exemplo de uso do flexbox

--- esquenta/1-masterclass-react-next/src/app/page.tsx
+++ esquenta/1-masterclass-react-next/src/app/page.tsx
@@ -2,10 +2,16 @@ import Link from "next/link";
 
 export default function Inicio() {
   return (
-    <div>
-      <h1>Masterclass Next</h1>
-      <nav>
+    <div
+      className="
+        min-h-screen
+        bg-gradient=to-r from-black to-zink-800
+      "
+    >
+      <h1 className="font-black">Masterclass Next</h1>
+      <nav className="flex flex-col">
         <Link href={"/primeiro"}>Primeiro Componente</Link>
+        <Link href={"/flexbox"}>Flexbox</Link>
       </nav>
     </div>
   );
