ee8240991ad91bed9ab60fc7ab1263b554a35625
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-20 21:53:53 -0300

    Hook personalizado

--- esquenta/2-marterclass-reactnative/app/formulario.tsx
+++ esquenta/2-marterclass-reactnative/app/formulario.tsx
@@ -2,39 +2,10 @@ import { Text, TextInput, View } from 'react-native';
 import Botao from '@/components/Botao';
 import Cpf from '@/utils/Cpf';
 import styles from '@/constants/styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect, useState } from 'react';
+import useFormUsuario from '@/hooks/useFormUsuario';
 
 export default function TelaFormulario() {
-    const [usuario, setUsuario] = useState<any>({})
-    const [erros, setErros] = useState<any>({})
-
-    useEffect(() => {
-        AsyncStorage.getItem('usuario').then((usuario) => {
-            if (usuario) {
-                setUsuario(JSON.parse(usuario))
-            }
-        })
-    }, [])
-
-    function validar() {
-        let erros = {}
-
-        if (!usuario.nome) {
-            erros = { ...erros, nome: 'Nome é obrigatório' }
-        } else if (usuario.nome.length < 3) {
-            erros = { ...erros, nome: 'Nome deve ter no mínimo 3 caracteres' }
-        }
-
-        setErros(erros)
-        return Object.keys(erros).length === 0
-    }
-
-    function salvar() {
-        if (validar()) {
-            AsyncStorage.setItem('usuario', JSON.stringify(usuario))
-        }
-    }
+    const { usuario, erros, setUsuario, salvar } = useFormUsuario()
 
     return (
         <View style={styles.centralizado}>
