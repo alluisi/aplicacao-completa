19f32b2501eeb8d26ea9010c1976bdbea4fe27f4
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-21 00:52:15 -0300

    Criação do repositório de usuário

--- esquenta/3-masterclass-nestjs/src/usuario/usuario.controller.ts
+++ esquenta/3-masterclass-nestjs/src/usuario/usuario.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import Usuario from './usuario.entity';
+import { UsuarioRepository } from './usuario.repository';
 
 @Controller('usuario')
 export class UsuarioController {
+  constructor(private repo: UsuarioRepository) {}
+
   @Post()
   async criar(@Body() usuario: Usuario) {
-    return { ...usuario, id: 1000 };
+    const novoUsuario = await this.repo.criar(usuario);
+    return novoUsuario;
   }
 }
