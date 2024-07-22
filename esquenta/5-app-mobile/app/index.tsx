67ca87a90bd0e522bacc7b684373140320e05a5b
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-22 01:54:35 -0300

    Criação do componente de resultado do questionário

--- esquenta/5-app-mobile/app/index.tsx
+++ esquenta/5-app-mobile/app/index.tsx
@@ -3,15 +3,25 @@ import Logo from '@/components/template/Logo'
 import Pagina from '@/components/template/Pagina'
 import Pergunta from '@/components/questionario/Pergunta'
 import useQuestionario from '@/data/hooks/useQuestionario'
+import Resultado from '@/components/questionario/Resultado'
 
 export default function Index() {
-    const { pergunta, responder } = useQuestionario()
+    const { pergunta, concluido, pontuacao, totalDePerguntas, responder, reiniciar } =
+        useQuestionario()
 
     return (
         <Pagina>
             <View style={{ gap: 40 }}>
                 <Logo />
-                <Pergunta pergunta={pergunta} opcaoSelecionada={responder} />
+                {concluido ? (
+                    <Resultado
+                        pontuacao={pontuacao}
+                        totalDePerguntas={totalDePerguntas}
+                        reiniciar={reiniciar}
+                    />
+                ) : (
+                    <Pergunta pergunta={pergunta} opcaoSelecionada={responder} />
+                )}
             </View>
         </Pagina>
     )
