54b911f4ca925e1b0c355afc0239d35e81c857f6
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-21 23:05:44 -0300

    Criação do componente carrinho

--- esquenta/4-app-web/src/app/(loja)/carrinho/page.tsx
+++ esquenta/4-app-web/src/app/(loja)/carrinho/page.tsx
@@ -1,6 +1,21 @@
 'use client'
-import Pagina from '@/components/template/Pagina';
+import AreaItemCarrinho from '@/components/carrinho/AreaItemCarrinho'
+import Pagina from '@/components/template/Pagina'
+import useCarrinho from '@/data/hooks/UseCarrinho'
 
 export default function PaginaCarrinho() {
-    return <Pagina>Carrinho</Pagina>
+    const { itens, adicionar } = useCarrinho()
+    return (
+        <Pagina className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5">
+                {itens.map((item) => (
+                    <AreaItemCarrinho
+                        key={item.produto.id}
+                        item={item}
+                        adicionar={(item) => adicionar(item.produto)}
+                    />
+                ))}
+            </div>
+        </Pagina>
+    )
 }
