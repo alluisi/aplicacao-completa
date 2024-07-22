442ed7b89a797e245ead51ca1ed9b5ba828e2696
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-21 21:32:32 -0300

    Criação de contexto

---
+++ esquenta/4-app-web/src/app/(loja)/carrinho/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import Pagina from '@/components/template/Pagina';
+import ContextoCarrinho from '@/data/contexts/ContextoCarrinho';
+import { useContext } from 'react';
+
+export default function PaginaCarrinho() {
+    const { numero } = useContext(ContextoCarrinho)
+
+    return <Pagina>Carrinho: {numero}</Pagina>
+}
