72baa4bf43cba99b69dff36f5bf8996fd0e6e991
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-21 22:29:57 -0300

    Adição de itens no carrinho

--- esquenta/4-app-web/src/app/(loja)/carrinho/page.tsx
+++ esquenta/4-app-web/src/app/(loja)/carrinho/page.tsx
@@ -1,9 +1,6 @@
 'use client'
 import Pagina from '@/components/template/Pagina';
-import useCarrinho from '@/data/hooks/UseCarrinho';
 
 export default function PaginaCarrinho() {
-    const { numero } = useCarrinho()
-
-    return <Pagina>Carrinho: {numero}</Pagina>
+    return <Pagina>Carrinho</Pagina>
 }
--- esquenta/4-app-web/src/components/template/Carrinho.tsx
+++ esquenta/4-app-web/src/components/template/Carrinho.tsx
@@ -1,18 +1,20 @@
+import useCarrinho from '@/data/hooks/UseCarrinho'
 import { IconShoppingCart } from '@tabler/icons-react'
 import Link from 'next/link'
 
 export default function Carrinho() {
+    const { qtdeDeItens } = useCarrinho()
     return (
         <Link href="/carrinho">
             <div className="flex relative">
                 <IconShoppingCart size={32} stroke={1} />
                 <div
                     className="
-                        absolute -top-2.5 -right-2.5 w-6 h-6 bg-red-600 rounded-full
+                        absolute -top-2.5 -right-2.5 w-6 h-6 bg-red-500 rounded-full
                         flex justify-center items-center text-xs text-white
                     "
                 >
-                    99
+                    {qtdeDeItens}
                 </div>
             </div>
         </Link>
