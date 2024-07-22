7d6055cdca02d343bd38220e7e9ddce5878fd166
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-22 00:52:54 -0300

    Criação do componente de template da logo

--- esquenta/5-app-mobile/app/index.tsx
+++ esquenta/5-app-mobile/app/index.tsx
@@ -1,12 +1,10 @@
+import Logo from "@/components/template/Logo";
 import Pagina from "@/components/template/Pagina";
-import { Text } from "react-native";
 
 export default function Index() {
   return (
     <Pagina>
-      <Text style={{ fontFamily: 'SPACEMISSION', color: '#fff' }}>
-        Teste da fonte SPACEMISSION
-      </Text>
+      <Logo />
     </Pagina>
   );
 }
