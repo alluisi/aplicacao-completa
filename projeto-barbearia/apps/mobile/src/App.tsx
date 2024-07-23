f422b135ef95bf3a0d68dc1c651d0f45d963ee34
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-22 22:20:47 -0300

    Teste do core das aplicações frontend e mobile

--- projeto-barbearia/apps/mobile/src/App.tsx
+++ projeto-barbearia/apps/mobile/src/App.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native'
+import { somar } from '@barba/core'
 
 export default function App() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Mobile</Text>
+            <Text>Mobile {somar(32, 48)}</Text>
         </View>
     )
 }
