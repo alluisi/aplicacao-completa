2b6aaf1a0e21a9875f76e1cfc6878c5c72ef2c4b
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-27 20:50:54 -0300

    Inserção da lógica de forçar login do usuário

--- projeto-barbearia/apps/frontend/src/app/(paginas)/(internas)/layout.tsx
+++ projeto-barbearia/apps/frontend/src/app/(paginas)/(internas)/layout.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { ProvedorAgendamento } from '@/data/contexts/ContextoAgendamento'
-// import ForcarUsuario from '@/components/shared/ForcarUsuario'
+import ForcarUsuario from '@/components/shared/ForcarUsuario'
 import Pagina from '@/components/shared/Pagina'
 
 export default function Layout(props: any) {
     return (
-        // <ForcarUsuario>
+        <ForcarUsuario>
             <ProvedorAgendamento>
                 <Pagina>{props.children}</Pagina>
             </ProvedorAgendamento>
-        // </ForcarUsuario>
+        </ForcarUsuario>
     )
 }
