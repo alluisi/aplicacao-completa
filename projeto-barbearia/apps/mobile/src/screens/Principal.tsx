66af72301d891504d091b4c4fa4d8225b270e7ed
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2024-07-28 15:57:55 -0300

    Estrutura inicial para navegação

---
+++ projeto-barbearia/apps/mobile/src/screens/Principal.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Principal() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Principal</Text>
+        </View>
+    )
+}
